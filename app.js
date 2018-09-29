7bb0a36e67ce3fc71e942356976d2ceba37c52c2
Doron Mor <mobclown@gmail.com>
2018-09-29 12:24:02 +0300

    add generator and dictionary

--- app.js
+++ app.js
@@ -1,76 +1,48 @@
 var app = new Vue({
-  el: '#app',
-  data: {
-    name: 'X-ray',
-    nouns: [
-        'zero',
-        'acceleration',
-        'Aerolite',
-        'Airglow',
-        'albedo',
-        'Apastron',
-        'Aphelion',
-        'Apogee',
-        'Asteroid',
-        'Atmosphere',
-        'Atom',
-        'Aurora',
-        'star',
-        'Black Hole',
-        'Bolide',
-        'Chromosphere',
-        'Cluster',
-        'comet',
-        'Corona',
-        'ray',
-        'X-ray',
-        'Eclipse',
-        'Ecosphere',
-        'Electron',
-        'Exosphere',
-        'Galaxy',
-        'Planet',
-        'Light Year',
-        'Meteor',
-        'Meteorite',
-        'Nebula',
-        'Neutrino',
-        'Nova',
-        'Occultation',
-        'Parallax',
-        'Periastron',
-        'Perigee',
-        'Perihelion',
-        'Photosphere',
-        'Quasar',
-        'Satellite',
-        'Scintillation',
-        'Solstice',
-        'Spicules',
-        'Supernova',
-        'Terminator',
-        'Troposphere',
-        'nucleus',
-        'sky',
-        'matter',
-        'Fireball',
-        'Hadron',
-    ],
-    adjectives: [
-        'absolute',
-        'Annular',
-        'Celestial',
-        'Circumpolar',
-        'cosmic',
-        'Solar',
-        'Geocentric',
-        // 'Gibbous',
-        'inferior',
-        'sidereal',
-        'Superior',
-        'dark',
-        'Lunar',
+    el: '#app',
+    data: {
+        name: '',
+        nouns: nouns,
+        adjectives: adjectives,
+        isShowCopyBtn: false,
+    },
+    created() {
+        this.generate();
+    },
+    methods: {
+        generate() {
+            let adjective = this.adjectives[this.random(this.adjectives.length)];
+            let noun = this.nouns[this.random(this.nouns.length)];
 
-    ],
-  }
-})
+            this.name = adjective + ' ' + noun;
+        },
+        random(n) {
+            return Math.floor(Math.random() * n);
+        },
+        showCopyBtn() {
+            this.isShowCopyBtn = true;
+        },
+        hideCopyBtn() {
+            this.isShowCopyBtn = false;
+        },
+        copy() {
+            this.copyStringToClipboard(this.name);
+        },
+        copyStringToClipboard(str) {
+            // Create new element
+            var el = document.createElement('textarea');
+            // Set value (string to be copied)
+            el.value = str;
+            // Set non-editable to avoid focus and move outside of view
+            el.setAttribute('readonly', '');
+            el.style = {position: 'absolute', left: '-9999px'};
+            document.body.appendChild(el);
+            // Select text inside element
+            el.select();
+            // Copy text to clipboard
+            document.execCommand('copy');
+            // Remove temporary element
+            document.body.removeChild(el);
+        },
+    }
+});
